1b98f8c91d8845dc8a81d3e25e2b0f1bc4ed1834
jeffft9 <diasjefferson2@gmail.com>
2020-04-17 17:55:14 +0530

    Learning Django
    Basic Register and Login Page using Django

---
+++ Django_Example/learning_templates/templatetags/my_extras.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='cut') #decorators
+def cut(value,arg):
+    """
+    This cuts out all values of "arg" from the string!
+    """
+    return value.replace(arg,'')
+
+# register.filter('cut',cut)
